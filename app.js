40134eb982273268b33066b8d53b5c27dc53ca3a
stevengalvis <steven.galvis@gmail.com>
2017-04-23 12:39:58 -0400

    final version

--- app.js
+++ app.js
@@ -1,52 +1,49 @@
 //create constructor function
 function ShoppingList() {
-    //change to an object? 
-    this.items = []
+    //change to an object?
+    this.items = {};
 };
 //create state object to hold shopping list
 var myList = new ShoppingList();
 
 var addItem = function(shoppingList, item) {
-    shoppingList.items.push(item);
+    shoppingList.items[item]= 'incomplete';
 }
 
 var deleteItem = function(shoppingList, item) {
-    var index = shoppingList.items.indexOf(item);
-    console.log(index);
-    shoppingList.items.splice(index, 1);
+    delete shoppingList.items[item];
 }
 
-var renderList = function(shoppingList, element) {
-    var itemsHtml = shoppingList.items.map(function(item) {
-        return "<li><span class = 'shopping-item'>" + item + "</span>" + "<div class ='shopping-item-controls'>" +
+function createHTML(item){
+    var itemHtml = "<li><span class = 'shopping-item'>" + item + "</span>" + "<div class ='shopping-item-controls'>" +
             '<button class = "shopping-item-toggle">' + '<span class="button-label">' + 'check</span>' +
             '</button>' + '<button class = "shopping-item-delete">' + '<span class = "shopping-item-delete">' +
             '<span class = "button-label">delete</span></button>' + '</div></li>';
-    });
-    element.html(itemsHtml);
+            return itemHtml;
 }
-//use append
-//use an object with properties checked?
+
+var createListItem = function(item, element) {
+    var itemHtml = createHTML(item);
+    element.append(itemHtml);
+}
 
 
 $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
-    addItem(myList, $('#shopping-list-entry').val());
-    renderList(myList, $('.shopping-list'));
+    var $listItem = $('#shopping-list-entry').val();
+    addItem(myList, $listItem);
+    createListItem($listItem, $('.shopping-list'));
 });
 
 
-$('.shopping-list').on("click", "li", "shopping-item-toggle", function(event) {
+$('.shopping-list').on("click", ".shopping-item-toggle", function(event) {
     event.preventDefault();
     $(this).toggleClass("shopping-item__checked");
 });
 
-
-
-
-
-// addItem(myList,'cat food');
-// addItem(myList,'dog food');
-// addItem(myList,'bunny food');
-// deleteItem(myList, 'cat food');
-// console.log(myList);
+$('.shopping-list').on("click",".shopping-item-delete",function(event){
+    event.preventDefault();
+    var $listItem = $('#shopping-list-entry').val();
+    deleteItem(myList,$listItem);
+    $(this).closest('li').remove();
+});
